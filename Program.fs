70ff8fa3547254bbd2f51fff91cccc85f328c91a
Mark Plunkett <mark.plunkett@cytecsolutions.com>
2020-12-17 13:47:07 +0000

    Fixed build issues in main solve function.

--- Program.fs
+++ Program.fs
@@ -1,37 +1,35 @@
 open System
 
-let isValid board row col =
+let isValid board =
     true
 
-let rec solve size (board:int[,]) row col =
+let rec solve size (board:Map<int, Map<int, int>>) row col =
 
     if row = size - 1 && col = size then
         Some board
-    if board.[row,col] <> 0 then
-        solve size board row (col + 1)
     else if col = size then
         solve size board (row + 1) 0
+    else if board.[row].[col] <> 0 then
+        solve size board row (col + 1)
     else
-        // we have a 0 so change 0 to each value, check validity and solve the next cell
-        [1..size]
-        |> Seq.
-        for i = 1 to size do
-            board.[row,col] <- i
-            if isValid board row col then
-                match solve size board row (col + 1) with
-                | Some b -> Some b
-                | _ -> None
-            
-            board.[row,col] <- 0
+        (*
+            We want to change the 0 to a new value
+            then try and solve with the board containing the new value
+        *)
 
-        None
+        [1..size]
+        |> Seq.map (fun i -> board.Add(row, board.[row].Add(col, i)))
+        |> Seq.filter isValid
+        |> Seq.tryPick (fun b -> solve size b row col)
 
 [<EntryPoint>]
 let main argv =
     let size = 9
     let solver = solve size
 
-    let grid = [
+    let inline listToMap list = list |> List.indexed |> Map.ofList
+    let listToBoard = (List.map listToMap) >> listToMap
+    let board = listToBoard [
         [3; 0; 6; 5; 0; 8; 4; 0; 0]
         [5; 2; 0; 0; 0; 0; 0; 0; 0]
         [0; 8; 7; 0; 0; 0; 0; 3; 1]
@@ -42,7 +40,7 @@ let main argv =
         [0; 0; 0; 0; 0; 0; 0; 7; 4]
         [0; 0; 5; 2; 0; 6; 3; 0; 0]
     ]
-    let board = Array2D.init size size (fun x y -> grid.[y].[x])
+
     match solve size board 0 0 with
     | Some solution -> ()
     | _ -> printfn "No solution"
