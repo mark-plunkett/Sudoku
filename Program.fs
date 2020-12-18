a126a132ee668bbe0ba6ba5115290e8889c9cb46
mark.plunkett <markje.p@gmail.com>
2020-12-18 09:06:32 +0000

    Added printBoard function.

--- Program.fs
+++ Program.fs
@@ -1,9 +1,57 @@
 open System
 
-let isValid board =
-    true
+open FSharpPlus
 
-let rec solve size (board:Map<int, Map<int, int>>) row col =
+type Board = Map<int, Map<int, int>>
+
+let boardToString =
+    Map.toSeq
+    >> Seq.map (
+        snd 
+        >> Map.toSeq 
+        >> Seq.map (snd >> string) 
+        >> String.concat " "
+    )
+    >> String.concat Environment.NewLine
+
+let isValid row col (board:Board) =
+
+    let uniqueOrZero =
+        Seq.groupBy id
+        >> Seq.forall (fun (v, g) -> v <> 0 && Seq.length g < 2)
+
+    let rowValid = 
+        board.[row]
+        |> Map.toSeq
+        |> Seq.map snd
+        |> uniqueOrZero
+
+    let colValid =
+        board
+        |> Map.toSeq
+        |> Seq.map (fun (_, map) -> map.[col])
+        |> uniqueOrZero
+
+    let rowMod = row % 3
+    let colMod = col % 3
+    let squareValid =
+        [ for j = rowMod to rowMod + 2 do
+            for i = colMod to colMod + 2 do
+                yield i, j
+        ]
+        |> Seq.map (fun (i, j) -> board.[j].[i])
+        |> uniqueOrZero
+    
+    //printfn "rowSum %i" rowSum
+    //printfn "colSum %i" colSum
+    //printfn "squareSum %i" squareSum
+
+    rowValid && colValid && squareValid
+
+let rec solve size (board:Board) row col =
+
+    printfn "%i x %i" row col
+    printfn "%s%s" (boardToString board) Environment.NewLine
 
     if row = size - 1 && col = size then
         Some board
@@ -15,11 +63,17 @@ let rec solve size (board:Map<int, Map<int, int>>) row col =
         (*
             We want to change the 0 to a new value
             then try and solve with the board containing the new value
+
+            for each value that can go in the cell
+            build a new board with the value at row,col
+            check whether it's valid according to sudoku rules
+            if yes then recurse to solve with the new board
+
         *)
 
         [1..size]
         |> Seq.map (fun i -> board.Add(row, board.[row].Add(col, i)))
-        |> Seq.filter isValid
+        |> Seq.filter (isValid row col)
         |> Seq.tryPick (fun b -> solve size b row col)
 
 [<EntryPoint>]
@@ -42,7 +96,7 @@ let main argv =
     ]
 
     match solve size board 0 0 with
-    | Some solution -> ()
+    | Some solution -> solution |> boardToString |> printfn "%s"
     | _ -> printfn "No solution"
 
     0
